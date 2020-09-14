3d28ce1fe23a198f59a0130fc49bfc4021233292
eric373 <ericryanco@gmail.com>
2020-09-14 13:30:59 -0400

    Added miscellaneous classes

---
+++ mylibs/misc/Event.java
@@ -0,0 +1,15 @@
+package mylibs.misc;
+
+// Represents an event.
+public class Event
+{
+    private String str;
+    public Event(String str) {this.str = str;}
+    public String toString() {return str;}
+
+    public static void main(String[] args)
+    {
+        Event e1 = new Event("Chicago");
+        System.out.println(e1.toString());
+    }
+}
