14d7b935ea40a4fac89c110b43e3ffcb726e604d
eric373 <ericryanco@gmail.com>
2020-10-05 01:27:54 -0400

    Added interface to define operations for all unordered symbol table implementations

---
+++ libs/algs/st/UnorderedSymbolTable.java
@@ -0,0 +1,13 @@
+package libs.algs.st;
+import java.util.Iterator;
+public interface UnorderedSymbolTable<Key, Value>
+{
+    public void put(Key key, Value value);
+    public Value get(Key key);
+    public void delete(Key key);
+    public boolean contains(Key key);
+    public boolean isEmpty();
+    public int size();
+    public Iterable<Key> keys();
+    public String toString();
+}
