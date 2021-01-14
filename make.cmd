4d229d13059fab4c84a06d397605f3c1f819e64f
eric373 <ericryanco@gmail.com>
2021-01-13 19:57:00 -0500

    Cleaned up unused files. Split batch script for compiling and running templates

--- make.cmd
+++ make.cmd
@@ -1,38 +1,6 @@
 cls
 
 :::: --------------------------------------------------
-::::  Testing and Experimentation
-:::: --------------------------------------------------
-::cd templates
-::javac HelloWorld.java
-::javac Basic.java
-::javac IterableTest.java
-::javac BasicRecursion.java
-::javac -d . Package1Class.java
-::javac -d ..\bin\ Package2Class.java
-::javac -d . Package3Class.java
-::jar cvf ..\bin\package3jar.jar .\package3\Package3Class.class
-::rmdir /Q/S package3
-::javac BasicReflection.java
-::javac GenericArrayCreation.java
-::javac GenericClass.java
-::
-::java HelloWorld
-::java Basic
-::java IterableTest
-::java BasicRecursion
-::java package1.Package1Class
-::java package2.Package2Class
-::java package3.Package3Class
-::java BasicReflection
-::java GenericArrayCreation
-::java GenericClass
-::
-::del /s .\*.class
-::cd ..
-
-
-:::: --------------------------------------------------
 ::::  Testing Textbook Code
 :::: --------------------------------------------------
 ::cd algs4
@@ -59,73 +27,73 @@ cls
 ::cd ..
 
 
-:: --------------------------------------------------
-::  Recompile Packages
-:: --------------------------------------------------
-:: Compile
-javac libs\combinatorics\CountingTechniques.java
-javac libs\util\Util.java
-javac libs\algs\Bag.java
-javac libs\algs\List.java
-javac libs\algs\Pair.java
-javac libs\algs\pq\MaxPQ.java
-javac libs\algs\pq\MinPQ.java
-javac libs\algs\pq\IndexMaxPQ.java
-javac libs\algs\Heapsort.java
-javac libs\algs\BinarySearch.java
-javac libs\algs\st\LinkedListSequentialSearchST.java
-javac libs\algs\st\ResizingArraySequentialSearchST.java
-javac libs\algs\st\ResizingArrayBinarySearchST.java
-javac libs\algs\st\LinkedListOrderedSequentialSearchST.java
-javac libs\algs\st\RecursiveBST.java
-javac libs\algs\st\BST.java
-javac libs\algs\st\RedBlackBST.java
-javac libs\algs\st\SymbolTable.java
-javac libs\algs\st\SeparateChainingHashST.java
-::javac libs\algs\st\LinearProbingHashST.java
-
-:: Archive
-del .\bin\libs.jar
-jar cvf ^
-.\bin\libs.jar ^
-.\libs\combinatorics\*.class ^
-.\libs\util\*.class ^
-.\libs\algs\pq\*.class ^
-.\libs\algs\st\*.class ^
-.\libs\algs\*.class
-
-:: Test
-java -ea libs.combinatorics.CountingTechniques
-java -ea libs.util.Util
-java -ea libs.algs.Bag
-java -ea libs.algs.List
-java -ea libs.algs.Pair
-java -ea libs.algs.pq.MaxPQ < libs\algs\input.txt
-java -ea libs.algs.pq.MinPQ < libs\algs\input.txt
-java -ea libs.algs.pq.IndexMaxPQ < libs\algs\input.txt
-java -ea libs.algs.Heapsort
-java -ea libs.algs.BinarySearch
-java -ea libs.algs.st.LinkedListSequentialSearchST -test
-java -ea libs.algs.st.LinkedListSequentialSearchST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.ResizingArraySequentialSearchST -test
-java -ea libs.algs.st.ResizingArraySequentialSearchST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.ResizingArrayBinarySearchST -test
-java -ea libs.algs.st.ResizingArrayBinarySearchST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.LinkedListOrderedSequentialSearchST -test
-java -ea libs.algs.st.LinkedListOrderedSequentialSearchST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.RecursiveBST -test
-java -ea libs.algs.st.RecursiveBST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.BST -test
-java -ea libs.algs.st.BST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.RedBlackBST -test
-java -ea libs.algs.st.RedBlackBST < libs\algs\STSampleinput.txt
-java -ea libs.algs.st.SeparateChainingHashST -test
-java -ea libs.algs.st.SeparateChainingHashST < algs4\tale.txt
-java -ea libs.algs.st.SeparateChainingHashST -resizeTest < algs4\tale.txt
-java -ea libs.algs.st.LinearProbingHashST -test
-
-:: Clean
-del /s .\libs\*.class
+:::: --------------------------------------------------
+::::  Recompile Packages
+:::: --------------------------------------------------
+:::: Compile
+::javac libs\combinatorics\CountingTechniques.java
+::javac libs\util\Util.java
+::javac libs\algs\Bag.java
+::javac libs\algs\List.java
+::javac libs\algs\Pair.java
+::javac libs\algs\pq\MaxPQ.java
+::javac libs\algs\pq\MinPQ.java
+::javac libs\algs\pq\IndexMaxPQ.java
+::javac libs\algs\Heapsort.java
+::javac libs\algs\BinarySearch.java
+::javac libs\algs\st\LinkedListSequentialSearchST.java
+::javac libs\algs\st\ResizingArraySequentialSearchST.java
+::javac libs\algs\st\ResizingArrayBinarySearchST.java
+::javac libs\algs\st\LinkedListOrderedSequentialSearchST.java
+::javac libs\algs\st\RecursiveBST.java
+::javac libs\algs\st\BST.java
+::javac libs\algs\st\RedBlackBST.java
+::javac libs\algs\st\SymbolTable.java
+::javac libs\algs\st\SeparateChainingHashST.java
+::::javac libs\algs\st\LinearProbingHashST.java
+::
+:::: Archive
+::del .\bin\libs.jar
+::jar cvf ^
+::.\bin\libs.jar ^
+::.\libs\combinatorics\*.class ^
+::.\libs\util\*.class ^
+::.\libs\algs\pq\*.class ^
+::.\libs\algs\st\*.class ^
+::.\libs\algs\*.class
+::
+:::: Test
+::java -ea libs.combinatorics.CountingTechniques
+::java -ea libs.util.Util
+::java -ea libs.algs.Bag
+::java -ea libs.algs.List
+::java -ea libs.algs.Pair
+::java -ea libs.algs.pq.MaxPQ < libs\algs\input.txt
+::java -ea libs.algs.pq.MinPQ < libs\algs\input.txt
+::java -ea libs.algs.pq.IndexMaxPQ < libs\algs\input.txt
+::java -ea libs.algs.Heapsort
+::java -ea libs.algs.BinarySearch
+::java -ea libs.algs.st.LinkedListSequentialSearchST -test
+::java -ea libs.algs.st.LinkedListSequentialSearchST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.ResizingArraySequentialSearchST -test
+::java -ea libs.algs.st.ResizingArraySequentialSearchST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.ResizingArrayBinarySearchST -test
+::java -ea libs.algs.st.ResizingArrayBinarySearchST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.LinkedListOrderedSequentialSearchST -test
+::java -ea libs.algs.st.LinkedListOrderedSequentialSearchST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.RecursiveBST -test
+::java -ea libs.algs.st.RecursiveBST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.BST -test
+::java -ea libs.algs.st.BST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.RedBlackBST -test
+::java -ea libs.algs.st.RedBlackBST < libs\algs\STSampleinput.txt
+::java -ea libs.algs.st.SeparateChainingHashST -test
+::java -ea libs.algs.st.SeparateChainingHashST < algs4\tale.txt
+::java -ea libs.algs.st.SeparateChainingHashST -resizeTest < algs4\tale.txt
+::java -ea libs.algs.st.LinearProbingHashST -test
+::
+:::: Clean
+::del /s .\libs\*.class
 
 
 
@@ -176,7 +144,7 @@ del /s .\libs\*.class
 
 ::cd exercises\1.4.10
 ::javac BinarySearchSmallestIndex.java
-::java BinarySearchSmallestIndex ..\..\templates\8KIntsWithEvenReps.txt 2 4 6 8 1998
+::java BinarySearchSmallestIndex 8KIntsWithEvenReps.txt 2 4 6 8 1998
 ::cd ..\..
 
 ::cd exercises\1.5.01
---
+++ templates.cmd
@@ -0,0 +1,64 @@
+:: --------------------------------------------------------------------------------
+::
+:: This script demonstrates how to use the command-line interface cmd to compile
+:: and run Java programs quickly without the overhead of higher level concepts
+:: like projects in Eclipse.
+::
+:: --------------------------------------------------------------------------------
+
+:: --------------------------------------------------
+::  Testing and Experimentation
+:: --------------------------------------------------
+cd templates
+
+:: Compile and run a hello world program
+javac HelloWorld.java
+java HelloWorld
+
+:: Illustrates looping, conditionals, arrays and functions
+javac Basic.java
+java Basic
+
+:: Shows implementation of the Iterable and Iterator interface
+javac IterableTest.java
+java IterableTest
+
+:: Demonstrates basic recursion in Java
+javac BasicRecursion.java
+java BasicRecursion
+
+:: Compile and invoke a program within a package in the current directory
+:: NOTE: The package1 directory is created in the current directory
+javac -d . Package1Class.java
+java package1.Package1Class
+
+:: Compile into a different directory and invoke the program in a package located
+:: in another directory using the CLASSPATH environment variabl.e
+:: NOTE: CLASSPATH must include the absolute path of the ..\bin\ directory!
+:: NOTE: The package2 directory is created in ..\bin\
+javac -d ..\bin\ Package2Class.java
+java package2.Package2Class
+
+:: Compile and add to a jar. Delete the package3 directory and invoke the
+:: program through the jar.
+:: NOTE: The aboslute path of ..\jar\package3jar.jar must be included in CLASSPATH.
+javac -d . Package3Class.java
+jar cvf ..\jar\package3jar.jar .\package3\Package3Class.class
+rmdir /Q/S package3
+java package3.Package3Class
+
+:: Demonstrates the use of Reflection in Java
+javac BasicReflection.java
+java BasicReflection
+
+:: Shows how to create an array with a type that uses generics
+javac GenericArrayCreation.java
+java GenericArrayCreation
+
+:: Shows a simple class with generic types
+javac GenericClass.java
+java GenericClass
+
+:: Clean all files and return to the directory above
+del /s .\*.class
+cd ..
