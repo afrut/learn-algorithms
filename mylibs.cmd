762360f0dfa6a1530008fa11873b549ad4c955f4
eric373 <ericryanco@gmail.com>
2021-03-01 17:57:58 -0500

    Depth-first search implementation of cycle detection in graphs

--- mylibs.cmd
+++ mylibs.cmd
@@ -62,6 +62,8 @@ cls
 ::javac src\main\java\mylibs\GraphPathsRecursiveDFS.java -d .\
 ::javac src\main\java\mylibs\GraphPathsBFS.java -d .\
 ::javac src\main\java\mylibs\GraphProperties.java -d .\
+::javac src\main\java\mylibs\GraphConnectDFS.java -d .\
+javac src\main\java\mylibs\GraphCyclesDFS.java -d .\
 ::
 :::: Create a jar in .\jar\ from bytecode .class files in .\mylibs
 ::del .\jar\mylibs.jar
@@ -130,6 +132,10 @@ cls
 ::java -ea mylibs.GraphPathsBFS src\main\resources\tinyGex2.txt -trace
 ::java -ea mylibs.GraphProperties src\main\resources\myMediumG.txt
 ::java -ea mylibs.GraphProperties src\main\resources\tinyGex2.txt
+::java -ea mylibs.GraphConnectDFS src\main\resources\myMediumG.txt
+::java -ea mylibs.GraphConnectDFS src\main\resources\tinyGex2.txt
+java -ea mylibs.GraphCyclesDFS src\main\resources\myMediumG.txt
+java -ea mylibs.GraphCyclesDFS src\main\resources\tinyGex2.txt
 
 :: Clean
 del /s .\mylibs\*.class
---
+++ src/main/java/mylibs/GraphCyclesDFS.java
@@ -0,0 +1,48 @@
+package mylibs;
+
+import java.io.FileNotFoundException;
+import mylibs.Util;
+import java.util.LinkedList;
+
+public class GraphCyclesDFS
+{
+    private Graph graph;
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public GraphCyclesDFS(Graph graph)
+    {
+        this.graph = graph;
+        marked = new boolean[graph.V()];
+        for(int v = 0; v < graph.V(); v++)
+            dfs(v, v);
+    }
+
+    // u is the vertex from which v is reached
+    // v is the vertex being visited
+    // w is the vertex to be visited next
+    private void dfs(int v, int u)
+    {
+        marked[v] = true;
+        for(int w : graph.adj(v))
+            if(!marked[w]) dfs(w, v);
+            else if(w != u) hasCycle = true;
+                // w has been visited before and it is not the vertex used to
+                // reach v, so there is a cycle!
+    }
+
+    // return if graph is acyclic
+    public boolean hasCycle()
+    {return hasCycle;}
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        String[] in = Util.fromFile(args[0]);
+        Integer[] a = new Integer[in.length];
+        for(int cnt = 0; cnt < in.length; cnt++)
+            a[cnt] = Integer.parseInt(in[cnt]);
+        Graph graph = new Graph(a, false, false);
+        GraphCyclesDFS gcd = new GraphCyclesDFS(graph);
+        System.out.println("Graph has cycles? " + gcd.hasCycle());
+    }
+}
