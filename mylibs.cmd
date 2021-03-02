245fe7c94f9a18f2ad30c8ef0f4e8bdcc6d96214
eric373 <ericryanco@gmail.com>
2021-03-01 20:37:01 -0500

    Breadth-first search-based implemenation for cycle detection. Implemented function for finding the girth of a graph. Completed 4.1.18

--- mylibs.cmd
+++ mylibs.cmd
@@ -63,7 +63,8 @@ cls
 ::javac src\main\java\mylibs\GraphPathsBFS.java -d .\
 ::javac src\main\java\mylibs\GraphProperties.java -d .\
 ::javac src\main\java\mylibs\GraphConnectDFS.java -d .\
-javac src\main\java\mylibs\GraphCyclesDFS.java -d .\
+::javac src\main\java\mylibs\GraphCyclesDFS.java -d .\
+javac src\main\java\mylibs\GraphCyclesBFS.java -d .\
 ::
 :::: Create a jar in .\jar\ from bytecode .class files in .\mylibs
 ::del .\jar\mylibs.jar
@@ -134,8 +135,10 @@ javac src\main\java\mylibs\GraphCyclesDFS.java -d .\
 ::java -ea mylibs.GraphProperties src\main\resources\tinyGex2.txt
 ::java -ea mylibs.GraphConnectDFS src\main\resources\myMediumG.txt
 ::java -ea mylibs.GraphConnectDFS src\main\resources\tinyGex2.txt
-java -ea mylibs.GraphCyclesDFS src\main\resources\myMediumG.txt
-java -ea mylibs.GraphCyclesDFS src\main\resources\tinyGex2.txt
+::java -ea mylibs.GraphCyclesDFS src\main\resources\myMediumG.txt
+::java -ea mylibs.GraphCyclesDFS src\main\resources\tinyGex2.txt
+java -ea mylibs.GraphCyclesBFS src\main\resources\myMediumG.txt
+java -ea mylibs.GraphCyclesBFS src\main\resources\tinyGex2.txt
 
 :: Clean
 del /s .\mylibs\*.class
---
+++ src/main/java/mylibs/GraphCyclesBFS.java
@@ -0,0 +1,76 @@
+package mylibs;
+
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import mylibs.Util;
+
+public class GraphCyclesBFS
+{
+    private Graph graph;
+    private boolean hasCycle;
+    private int girth;
+    
+    public GraphCyclesBFS(Graph graph)
+    {
+        this.graph = graph;
+        hasCycle = false;
+        girth = -1;
+        for(int v = 0; v < graph.V(); v++)
+            bfs(v);
+    }
+    
+    public void bfs(int s)
+    {
+        boolean[] marked = new boolean[graph.V()];
+        int[] edgeTo = new int[graph.V()];
+        int[] distTo = new int[graph.V()];
+        LinkedList<Integer>queue = new LinkedList<Integer>();
+        queue.add(s);
+        edgeTo[s] = s;
+        distTo[s] = 0;
+        
+        while(!queue.isEmpty())
+        {
+            int v = queue.poll();
+            marked[v] = true;
+            for(int w : graph.adj(v))
+            {
+                if(!marked[w])
+                {
+                    queue.add(w);
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                }
+                else if(edgeTo[v] != s && w == s)
+                {
+                    if(girth == -1) girth = distTo[v] + 1;
+                    else if(distTo[v] + 1 < girth)
+                    {
+                        girth = distTo[v] + 1;
+                        hasCycle = true;
+                    }
+                }
+            }          
+        }
+    }
+
+    // return if graph is acyclic
+    public boolean hasCycle()
+    {return hasCycle;}
+
+    // length of shortest cycle
+    public int girth()
+    {return girth;}
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        String[] in = Util.fromFile(args[0]);
+        Integer[] a = new Integer[in.length];
+        for(int cnt = 0; cnt < in.length; cnt++)
+            a[cnt] = Integer.parseInt(in[cnt]);
+        Graph graph = new Graph(a, false, false);
+        GraphCyclesBFS gcb = new GraphCyclesBFS(graph);
+        System.out.println("Graph has cycles? " + gcb.hasCycle());
+        System.out.println("girth = " + gcb.girth());
+    }
+}
