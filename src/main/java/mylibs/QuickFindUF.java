999557c85af491c385023f08bf1088947522d18f
eric373 <ericryanco@gmail.com>
2021-05-29 12:14:46 -0400

    Added comments

--- src/main/java/mylibs/QuickFindUF.java
+++ src/main/java/mylibs/QuickFindUF.java
@@ -1,53 +1,69 @@
 package mylibs;
 import java.io.FileNotFoundException;
-import mylibs.Util;
+
+// Dynamic connectivity problem:
+// A number of vertices can have connections. Connected vertices form a component.
+// Support the following operations efficiently:
+// - connect a component p and q
+// - identify the component that a vertex belongs to
+// - check if two vertices are connected
+
+// The quick-find algorithm uses a vertex-indexed array id. The core idea is to
+// have id[p] contain the top-most parent at all times. So, the find operation
+// simply returns id[p], hence quick-find. The union operation does the heavy
+// lifting. When two vertices p and q are to be connected, union would have:
+// - to loop through every vertex v in id
+// - check if the parent of each vertex is the parent of p; id[v] == id[p]?
+// - if true, then set the parent of the vertex to the parent of q; id[v] = id[q]
 
 public class QuickFindUF
 {
-    private int[] id; // parent link (site indexed)
-    private int count; // number of components
+    private int[] id;       // for a vertex v, id[p] gives the parent vertex of v
+    private int count;      // number of components
 
     public QuickFindUF(int N)
     {
-        count = N;
-        id = new int[N];
-        for (int i = 0; i < N; i++)
+        count = N;                      // start with N components
+        id = new int[N];                // initialize
+        for (int i = 0; i < N; i++)     // every vertex's parent is itself
             id[i] = i;
     }
 
-    public int count()
-    {
-        return count;
-    }
+    // return the number of components
+    public int count() {return count;}
 
-    public boolean connected(int p, int q)
-    {
-        return find(p) == find(q);
-    }
+    // return the component id of a vertex
+    // constant operation: 1 array access
+    public int find(int p) {return id[p];}
 
-    public int find(int p) { return id[p]; }
+    // check if two vertices are connected
+    // constant operation: 2 array accesses
+    public boolean connected(int p, int q) {return find(p) == find(q);}
 
+    // connect p and q
+    // linear operation: at least 2 + N + 1, at most 2 + N + N - 1
     public void union(int p, int q)
-    { // Put p and q into the same component.
-        int pID = find(p);
-        int qID = find(q);
-        // Nothing to do if p and q are already in the same component.
+    {
+        int pID = find(p);      // 1 array access
+        int qID = find(q);      // 1 array access
+
+        // if already connected, do nothing
         if (pID == qID) return;
-        // Rename p’s component to q’s name.
+
+        // Set the parent of all vertices whose parent is the parent of p to the
+        // parent of q.
+        // This is the inner loop of quick find. Linear in time.
         for (int i = 0; i < id.length; i++)
-            if (id[i] == pID) id[i] = qID;
+            if(id[i] == pID)    // N array accesses
+                id[i] = qID;    // at least 1 array access. at most, N - 1
         count--;
     }
 
     public String toString()
     {
         StringBuilder sb = new StringBuilder();
-        for(Integer x : id)
-        {
-            sb.append(x + " ");
-        }
-        if(id.length > 0)
-            sb.setLength(sb.length() - 1);
+        for(Integer x : id) sb.append(x + " ");
+        if(id.length > 0) sb.setLength(sb.length() - 1);
         return sb.toString();
     }
 
